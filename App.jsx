1a803448f8849167566ad52be3f9251a3703f4c3
cloutxcoin <DAWNGOSSELIN80@GMAIL.COM>
2025-05-16 14:03:36 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div style={{ backgroundColor: 'black', color: 'lime', fontFamily: 'monospace', textAlign: 'center', padding: '50px' }}>
+      <h1>$PIMPJUICE Presale</h1>
+      <p>Phantom wallet connection and buy flow will be added here.</p>
+    </div>
+  )
+}
+
+export default App
